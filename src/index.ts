b85c16eed100232690ee0e13e48e3fd3caf041f1
Igor Kamyshev <igor@kamyshev.me>
2020-04-30 19:18:58 +0700

    feat: export transaction interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { MrSolomons } from './services/MrSolomons';
 export { MrButcher } from './services/MrButcher';
 export { DetBell } from './services/DetBell';
+
+export { Transaction } from './types/Transaction';
