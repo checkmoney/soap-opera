be7d3612e1c1b81a8ca91332bec26a1b7027b9c5
Igor Kamyshev <igor@kamyshev.me>
2020-04-30 18:58:14 +0700

    feat(det-bell): add method for fetching default currency

--- src/services/DetBell.ts
+++ src/services/DetBell.ts
@@ -1,12 +1,41 @@
+import Axios, { AxiosInstance } from 'axios';
 import * as jwt from 'jsonwebtoken';
 
-// TODO: It is a fake client, rewrite it!
-export class DetBell {
-  constructor(private readonly appSecret: string) {}
+import { addTokenToHttpConfig } from './utils/addTokenToHttpConfig';
+import { InvalidResponseException } from './exceptions/InvalidResponseException';
 
+export class DetBell {
+  private readonly http: AxiosInstance;
+
+  constructor(private readonly appSecret: string, url: string) {
+    const serviceUrl = url;
+
+    this.http = Axios.create({
+      baseURL: serviceUrl,
+    });
+  }
+
+  // TODO: It is a fake method, rewrite it!
   async pretend(userId: string): Promise<string> {
     const payload = { login: userId, isManager: false };
 
     return jwt.sign(payload, this.appSecret, { algorithm: 'HS256' });
+  }
+
+  async getDefaultCurrency(token: string): Promise<string> {
+    const { data: defaultCurrency } = await this.http.get<string>(
+      `/v1/default-currency`,
+      addTokenToHttpConfig(token, {}),
+    );
+
+    if (typeof defaultCurrency !== 'string' || defaultCurrency.length !== 3) {
+      throw new InvalidResponseException(
+        'String with 3 symbols',
+        defaultCurrency,
+        'det-bell',
+      );
+    }
+
+    return defaultCurrency;
   }
 }
